697ee0c8c46d0162cd9725e116229bfb0d7ce5ae
liam.watts <liam.watts@thetradedesk.com>
2023-12-03 20:38:26 -0500

    day1: solution to part1

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,1 +1,7 @@
-Console.WriteLine("Hello World");
+Console.WriteLine(Solution());
+
+static IEnumerable<string> ReadInput() => File.ReadLines("../puzzleinput/day1.txt");
+
+static int CalculateCalibration(string line) => int.Parse($"{line.First(char.IsDigit)}{line.Last(char.IsDigit)}");
+
+static int Solution() => ReadInput().Select(CalculateCalibration).Sum();
