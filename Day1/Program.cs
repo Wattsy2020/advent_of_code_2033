8586b384e810fa3d313bc41fdb302025a5bfd99d
liam.watts <liam.watts@thetradedesk.com>
2023-12-03 21:14:22 -0500

    day1: part2 solution

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,7 +1,43 @@
+Dictionary<string, int> digitMapping = new()
+{
+    { "one", 1 },
+    { "two", 2 },
+    { "three", 3 },
+    { "four", 4 },
+    { "five", 5 },
+    { "six", 6 },
+    { "seven", 7 },
+    { "eight", 8 },
+    { "nine", 9 },
+};
+const int minDigitLength = 3;
+const int maxDigitLength = 5;
 Console.WriteLine(Solution());
+return;
 
 static IEnumerable<string> ReadInput() => File.ReadLines("../puzzleinput/day1.txt");
 
-static int CalculateCalibration(string line) => int.Parse($"{line.First(char.IsDigit)}{line.Last(char.IsDigit)}");
+static int ConcatDigits(int digit1, int digit2) => digit1 * 10 + digit2;
 
-static int Solution() => ReadInput().Select(CalculateCalibration).Sum();
+int? FindDigit(string line, int index)
+{
+    if (int.TryParse(line[index..(index+1)], out int result))
+        return result;
+    int maxLength = Math.Min(maxDigitLength, line.Length - index);
+    for (int i = minDigitLength; i <= maxLength; i++)
+        if (digitMapping.TryGetValue(line[index .. (index + i)], out result))
+            return result;
+    return null;
+}
+
+int FirstDigit(string line, IEnumerable<int> indexes)
+    => indexes.Select(idx => FindDigit(line, idx)).First(d => d.HasValue)!.Value;
+
+int CalculateCalibration(string line)
+{
+    var firstDigit = FirstDigit(line, Enumerable.Range(0, line.Length));
+    var lastDigit = FirstDigit(line, Enumerable.Range(0, line.Length).Reverse());
+    return ConcatDigits(firstDigit, lastDigit);
+}
+
+int Solution() => ReadInput().Select(CalculateCalibration).Sum();
